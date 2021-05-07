a42b0d31891537148d382a4586d18e554b1aae75
ashley <agahagan1@gmail.com>
2021-05-07 11:34:00 -0400

    adds initial seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "Hydrangea", price: 20, image_url: "https://www.gardeningknowhow.com/wp-content/uploads/2008/09/hydrangea.jpg", description:"Hydrangea flowers are produced from early spring to late autumn; they grow in flowerheads (corymbs or panicles) most often at the ends of the stems.")
+
+
+Product.create(name: "Peony", price: 50, image_url: "https://www.almanac.com/sites/default/files/styles/opengraph/public/image_nodes/peony-pink-e.jpg?itok=PCyvze1t", description:"Peonies are popular garden plants in temperate regions. Herbaceous peonies are also sold as cut flowers on a large scale, although generally only available in late spring and early summer.")
+
+Product.create(name: "Dahlia", price: 50, image_url: "https://www.nature-and-garden.com/wp-content/uploads/sites/4/2018/06/dahlia.jpg", description:"Dahlias are perennial plants with tuberous roots, though they are grown as annuals in some regions with cold winters. While some have herbaceous stems, others have stems which lignify in the absence of secondary tissue and resprout following winter dormancy, allowing further seasons of growth.")
+
+Product.create(name: "Begonia", price: 25, image_url: "https://hgic.clemson.edu/wp-content/uploads/2007/09/word-image.jpeg", description:"The Begonias are native to moist subtropical and tropical climates. Some species are commonly grown indoors as ornamental houseplants in cooler climates.")
