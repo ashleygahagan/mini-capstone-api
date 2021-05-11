638a41533e5f7453ee872c4300b861591c132a3f
ashley <agahagan1@gmail.com>
2021-05-11 13:40:27 -0400

    makes app well rested

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/all_products", controller: "products", action: "all_products"
-  get "/first_product", controller: "products", action: "first_product"
-  get "/second_product", controller: "products", action: "second_product"
-  get "/third_product", controller: "products", action: "third_product"
+  get "/products", controller: "products", action: "index"
+  post "/products", controller: "products", action: "create"
+  get "/products/:id", controller: "products", action: "show"
+  patch "/products/:id", controller: "products", action: "update"
+  delete "/products/:id", controller: "products", action: "destroy"
+ 
 end
